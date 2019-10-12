0e4b2adf90485a2cf8cb056a16748783a26293f4
QiweiZhang <42258456+QiweiZhang@users.noreply.github.com>
2019-10-12 15:01:18 +1100

    Update OneMinuteOneFile.py

--- OneMinuteOneFile.py
+++ OneMinuteOneFile.py
@@ -9,20 +9,21 @@ for i in range(len(dic)):
         a = f.readlines()
         for line in a:
             data = json.loads(line)
-            temp = {}
-            #temp["changedon"] = data["changedon"]
-            temp["hmacaddress"] = data["hmacaddress"]
-            temp["campus"] = data["campus"]
-            temp["building"] = data["building"]
-            temp["floor"] = data["floor"]
-            temp_json = json.dumps(temp)
-            temp_line = str(temp_json)+"\n"
+            if data["campus"] == "Parkville Indoor":
+                temp = {}
+                #temp["changedon"] = data["changedon"]
+                temp["hmacaddress"] = data["hmacaddress"]
+                #temp["campus"] = data["campus"]
+                temp["building"] = data["building"]
+                #temp["floor"] = data["floor"]
+                temp_json = json.dumps(temp)
+                temp_line = str(temp_json)+"\n"
 
-            dateArray = datetime.datetime.fromtimestamp(int(data["changedon"]))
-            dateStr = str(dateArray)
-            filename = dateStr[5:7]+dateStr[8:10]+dateStr[11:13]+dateStr[14:16]
+                dateArray = datetime.datetime.fromtimestamp(int(data["changedon"]))
+                dateStr = str(dateArray)
+                filename = dateStr[5:7]+dateStr[8:10]+dateStr[11:13]+dateStr[14:16]
 
-            o = open('PerMinute/'+filename, 'a')
-            o.write(temp_line)
-            o.close()
+                o = open('PerMinute/'+filename, 'a')
+                o.write(temp_line)
+                o.close()
     f.close()
