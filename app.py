339e81aaf8b0be0e914f2079dcf10ca354a2e131
Sid Zhang <zhangzheqitiny@yahoo.com>
2019-10-23 21:49:56 +1100

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,jsonify,request
 import sqlite3
 import json
 from collections import defaultdict
+from GeoInfo import buildlocationdict
+
 
 app = Flask(__name__)
 
@@ -14,137 +16,103 @@ def start():
         # userage = request.div.get("userage")
     return render_template("test_api.html")
 
-def geodev(property):
-    geojson = [{
-        data: {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": property[data]["coordinates"]
-            },
-            "properties":
-                {
-                    "building_name": data,
-                    "detail": {
-                        "PC": property[data]["PC"],
-                        "laptop": property[data]["laptop"],
-                        "mobile": property[data]["mobile"],
-                    },
-                    "total": property[data]["total"],
-                }
-        }
-    } for data in property]
 
-    return geojson
 
-def geocareer(property):
-    geojson =[{
-        data: {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": property[data]["coordinates"]
-            },
-            "properties":
-                {
-                    "building_name": data,
-                    "detail": {
-                        "student": property[data]["student"],
-                        "staff": property[data]["staff"],
-                    },
-                    "total": property[data]["total"],
-                }
-        }
-    } for data in property]
+def GetPeopleDistribution(time_s):
+    # time_s = "06301327"
+    conn = sqlite3.connect('DB3.sqlite')
 
-    return geojson
+    results = {}
+    time_max = int(time_s)
+    time_min = time_max - 100
+
+    c = conn.execute(
+        "SELECT building, career, count(*) FROM BInT LEFT OUTER JOIN Career on BInT.mac = Career.mac WHERE CAST(tm as int) > ? AND CAST(tm as int) <= ? GROUP BY building, career",
+        (time_min, time_max))
+    for row in c:
+        if row[0] in buildlocationdict:
+            if row[0] not in results:
+                results[row[0]] = {}
+                results[row[0]]["type"] = "Feature"
+                results[row[0]]["geometry"] = {}
+                results[row[0]]["geometry"]["type"] = "Point"
+                cds = buildlocationdict[row[0]]
+                results[row[0]]["geometry"]["coordinates"] = [cds[1], cds[0]]
+                results[row[0]]["properties"] = {}
+                results[row[0]]["properties"]["building_name"] =row[0]
+                results[row[0]]["properties"]["Student"] = 0
+                results[row[0]]["properties"]["Staff"] = 0
+                results[row[0]]["properties"]["total"] = 0
+
+            if row[1] == "student":
+                results[row[0]]["properties"]["Student"] += row[2]
+                results[row[0]]["properties"]["total"] += row[2]
+            elif row[1] == "staff":
+                results[row[0]]["properties"]["Staff"] += row[2]
+                results[row[0]]["properties"]["total"] += row[2]
+
+    conn.close()
+
+    return results
+
+def GetDeviceDistribution(time_s):
+    # time_s = "06301327"
+    conn = sqlite3.connect('DB3.sqlite')
+
+    results = {}
+    time_max = int(time_s)
+    time_min = time_max - 100
+
+    c = conn.execute(
+        "SELECT building, device, count(*) FROM BInT LEFT OUTER JOIN Device on BInT.mac = Career.mac WHERE CAST(tm as int) > ? AND CAST(tm as int) <= ? GROUP BY building, device",
+        (time_min, time_max))
+    for row in c:
+        print(row[0])
+        print(row[1])
+        if row[0] in buildlocationdict:
+            if row[0] not in results:
+                results[row[0]] = {}
+                results[row[0]]["type"] = "Feature"
+                results[row[0]]["geometry"] = {}
+                results[row[0]]["geometry"]["type"] = "Point"
+                cds = buildlocationdict[row[0]]
+                results[row[0]]["geometry"]["coordinates"] = [cds[1], cds[0]]
+                results[row[0]]["properties"] = {}
+                results[row[0]]["properties"]["building_name"] =row[0]
+                results[row[0]]["properties"]["PC"] = 0
+                results[row[0]]["properties"]["mobile"] = 0
+                results[row[0]]["properties"]["laptop"] = 0
+                results[row[0]]["properties"]["total"] = 0
+
+            if row[1] == "PC":
+                results[row[0]]["properties"]["PC"] += row[2]
+                results[row[0]]["properties"]["total"] += row[2]
+            elif row[1] == "laptop":
+                results[row[0]]["properties"]["laptop"] += row[2]
+                results[row[0]]["properties"]["total"] += row[2]
+            else:
+                results[row[0]]["properties"]["mobile"] += row[2]
+                results[row[0]]["properties"]["total"] += row[2]
+
+
+    conn.close()
+
+    return results
 
 @app.route('/users/', methods=['POST'])
 def add_user():
-    conn = sqlite3.connect('DB3.sqlite')
-    cursor = conn.cursor()
     type = request.json["mapType"]
-    time=request.json["date"]
-    print(time)
-    t=time[5:7]+time[8:10]+time[11:13]+time[14:16]
-    # te=time[5:7]+time[8:10]+time[11:13]+time[14:16]
+    time = request.json["date"]
+    # print(time)
+    t = time[5:7] + time[8:10] + time[11:13] + time[14:16]
     # print(t)
-    if type=="people":
-        cursor.execute("select building,career from BInT, Career where BInt.mac = Career.mac and tm= "+t+" ")
-        # data="po"
-        data = cursor.fetchall()
-        recordBudList=[]
-        property=defaultdict(defaultdict)
-        count=0
+    # t = "06301900"
+    if type == "people":
+    # t = time[5:7] + time[8:10] + time[11:13] + time[14:16]
 
-        for line in data:
-            # print(line[0])
-            if line[0] in recordBudList:
-
-                if line[1]=="staff":
-                    property[line[0]]["staff"] +=1
-                else:
-                    property[line[0]]["student"]+=1
-
-                property[line[0]]["total"]+=1
-
-            else:
-                recordBudList.append(line[1])
-                property[line[0]]["staff"] = 0
-                property[line[0]]["student"] = 0
-                property[line[0]]["total"]=0
-                property[line[0]]["coordinates"] = []
-
-                if line[1]=="staff":
-                    property[line[0]]["staff"] +=1
-                else:
-                    property[line[0]]["student"]+=1
-
-                property[line[0]]["total"] += 1
-
-
-            # print(property[data]["staff"])
-
-        geojson = geocareer(property)
-
-
+        geojson=GetPeopleDistribution(t)
     else:
-        cursor.execute("select building,device from BInT, Device where BInt.mac = Device.mac and tm="+t+" ")
-        data = cursor.fetchall()
-        recordBudList = []
-        property = defaultdict(defaultdict)
-        count = 0
-        for line in data:
-            # print(line[0])
-            if line[0] in recordBudList:
-
-                if line[1] == "laptop":
-                    property[line[0]]["laptop"] += 1
-                elif line[1] == "PC":
-                    property[line[0]]["PC"] += 1
-                else:
-                    property[line[0]]["mobile"] += 1
-                property[line[0]]["total"] += 1
-
-
-            else:
-                recordBudList.append(line[1])
-                property[line[0]]["laptop"] = 0
-                property[line[0]]["PC"] = 0
-                property[line[0]]["mobile"] = 0
-                property[line[0]]["total"] = 0
-                property[line[0]]["coordinates"]=[]
-
-                if line[1] == "laptop":
-                    property[line[0]]["laptop"] += 1
-                elif line[1] == "PC":
-                    property[line[0]]["PC"] += 1
-                else:
-                    property[line[0]]["mobile"] += 1
-
-                property[line[0]]["total"] += 1
-
-        geojson=geodev(property)
+        geojson =GetDeviceDistribution(t)
 
     return  json.dumps(geojson), 201
 # json.dumps(data)
