9bd7c338278fde61532b7ae04ff81d61b78d209d
QiweiZhang <42258456+QiweiZhang@users.noreply.github.com>
2019-10-31 14:38:07 +1100

    Update and rename data classify by MAC.py to PerMacPerFile.py

---
+++ PerMacPerFile.py
@@ -0,0 +1,34 @@
+# Write each record that belongs to a specific mac address to a file named as the mac address.
+
+import os
+import json
+import datetime
+import multiprocessing
+
+def permac(i,):
+    with open("split/"+i) as f:
+        a = f.readlines()
+        for line in a:
+            data = json.loads(line)
+            thisMac = data["hmacaddress"]
+            temp = {}
+            temp["changedon"] = data["changedon"]
+            temp["hapmacaddress"] = data["hapmacaddress"]
+            #temp["campus"] = data["campus"]
+            temp["building"] = data["building"]
+            #temp["floor"] = data["floor"]
+            temp_json = json.dumps(temp)
+            temp_line = str(temp_json)+"\n"
+
+            with open('classify/'+thisMac, 'a') as o:
+                o.writelines(temp_line)
+
+
+pool = multiprocessing.Pool(8)
+
+dic = (os.listdir("split"))
+for file in dic:
+    pool.apply_async(permac, (file,))
+
+pool.close()
+pool.join()
