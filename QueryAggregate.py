e14683ae9f9d6be32f67c5dd23291559b18cc673
QiweiZhang <42258456+QiweiZhang@users.noreply.github.com>
2019-11-05 09:41:33 +1100

    Update QueryAggregate.py

--- QueryAggregate.py
+++ QueryAggregate.py
@@ -40,116 +40,6 @@ def GetAggregatePathsTo(wt, wd, bn, st, et):
     results = {}
     cnt_res = {}
 
-    if wt == "teaching":
-        days = teaching_dict[wd]
-    elif wt == "exam":
-        days = exam_dict[wd]
-    else:
-        days = day_dict[wd]
-
-    results[0] = {}
-    results[0]["type"] = "Feature"
-    results[0]["geometry"] = {}
-    results[0]["geometry"]["type"] = "Point"
-    cds = buildlocationdict[building_no]
-    results[0]["geometry"]["coordinates"] = [cds[1], cds[0]]
-    results[0]["properties"] = {}
-    results[0]["properties"]["building_type"] = "start"
-    results[0]["properties"]["building_name"] = bn
-    results[0]["properties"]["total"] = 0
-
-    if wt == "teaching":
-        days = teaching_dict[wd]
-    else:
-        days = exam_dict[wd]
-
-    for day in days:
-        start_time = int(day) * 10000 + int(st)
-        end_time = int(day) * 10000 + int(et)
-        if int(st) < 100:
-            time_min = start_time - 7800
-        else:
-            time_min = start_time - 200
-        i = start_time
-        macs = {}
-        total = 0
-
-        while i > time_min:
-            try:
-                filePath = "PerMinute/0" + str(i)
-                with open(filePath) as f:
-                    a = f.readlines()
-                    for line in a:
-                        data = json.loads(line)
-                        if data["hmacaddress"] not in macs:
-                            macs[data["hmacaddress"]] = 0
-                            if data["building"] == building_no:
-                                macs[data["hmacaddress"]] = 1
-                                total += 1
-                f.close()
-            except:
-                error_cnt += 1
-            i -= 1
-
-        results[0]["properties"]["total"] += total
-
-        j = end_time
-        if end_time - start_time <= 200:
-            s_time = start_time
-        else:
-            if int(et) < 100:
-                s_time = end_time - 7800
-            else:
-                s_time = end_time - 200
-
-        while j > s_time:
-            try:
-                filePath = "PerMinute/0" + str(j)
-                with open(filePath) as f:
-                    a = f.readlines()
-                    for line in a:
-                        data = json.loads(line)
-                        if data["hmacaddress"] in macs and macs[data["hmacaddress"]] == 1:
-                            if data["building"] != building_no:
-                                if data["building"] not in cnt_res:
-                                    cnt_res[data["building"]] = 1
-                                else:
-                                    cnt_res[data["building"]] += 1
-                            macs[data["hmacaddress"]] = 0
-                f.close()
-            except:
-                error_cnt += 1
-            j -= 1
-
-
-    sorted_res = sorted(cnt_res.items(), key=lambda item:item[1], reverse=True)
-
-    idx = 1
-    for (building, cnt) in sorted_res:
-        if building in buildlocationdict and cnt_res[building] >= 20:
-            results[idx] = {}
-            results[idx]["type"] = "Feature"
-            results[idx]["geometry"] = {}
-            results[idx]["geometry"]["type"] = "Point"
-            cds = buildlocationdict[building]
-            results[idx]["geometry"]["coordinates"] = [cds[1], cds[0]]
-            results[idx]["properties"] = {}
-            results[idx]["properties"]["building_type"] = "destination"
-            results[idx]["properties"]["building_name"] = buildnamedict[building]
-            results[idx]["properties"]["change"] = cnt_res[building]
-            idx += 1
-
-            print(buildnamedict[building], cnt_res[building])
-
-    return results
-
-
-
-def GetAggregatePathsFrom(wt, wd, bn, st, et):
-    error_cnt = 0
-    results = {}
-    cnt_res = {}
-
     for num in buildnamedict:
         if buildnamedict[num] == bn:
             building_no = num
@@ -206,7 +96,7 @@ def GetAggregatePathsFrom(wt, wd, bn, st, et):
         if end_time - start_time <= 200:
             s_time = start_time
         else:
-            if int(et) < 200:
+            if int(et) < 100:
                 s_time = end_time - 7800
             else:
                 s_time = end_time - 200
@@ -230,6 +120,111 @@ def GetAggregatePathsFrom(wt, wd, bn, st, et):
                 error_cnt += 1
             j -= 1
 
+    print("total:", results[0]["properties"]["total"])
+
+    sorted_res = sorted(cnt_res.items(), key=lambda item:item[1], reverse=True)
+
+    idx = 1
+    for (building, cnt) in sorted_res:
+        if building in buildlocationdict and cnt_res[building] >= 20:
+            results[idx] = {}
+            results[idx]["type"] = "Feature"
+            results[idx]["geometry"] = {}
+            results[idx]["geometry"]["type"] = "Point"
+            cds = buildlocationdict[building]
+            results[idx]["geometry"]["coordinates"] = [cds[1], cds[0]]
+            results[idx]["properties"] = {}
+            results[idx]["properties"]["building_type"] = "destination"
+            results[idx]["properties"]["building_name"] = buildnamedict[building]
+            results[idx]["properties"]["change"] = cnt_res[building]
+            idx += 1
+
+            print(buildnamedict[building], cnt_res[building])
+
+    return results
+
+
+
+def GetAggregatePathsFrom(wt, wd, bn, st, et):
+    error_cnt = 0
+    results = {}
+    cnt_res = {}
+
+    for num in buildnamedict:
+        if buildnamedict[num] == bn:
+            building_no = num
+
+    results[0] = {}
+    results[0]["type"] = "Feature"
+    results[0]["geometry"] = {}
+    results[0]["geometry"]["type"] = "Point"
+    cds = buildlocationdict[building_no]
+    results[0]["geometry"]["coordinates"] = [cds[1], cds[0]]
+    results[0]["properties"] = {}
+    results[0]["properties"]["building_type"] = "start"
+    results[0]["properties"]["building_name"] = bn
+    results[0]["properties"]["total"] = 0
+
+    if wt == "teaching":
+        days = teaching_dict[wd]
+    elif wt == "exam":
+        days = exam_dict[wd]
+    else:
+        days = day_dict[wd]
+
+    for day in days:
+        start_time = int(day) * 10000 + int(st)
+        end_time = int(day) * 10000 + int(et)
+        total = 0
+        macs = {}
+
+        i = start_time
+
+        while i > end_time:
+            try:
+                filePath = "PerMinute/0" + str(i)
+                with open(filePath) as f:
+                    a = f.readlines()
+                    for line in a:
+                        data = json.loads(line)
+                        if data["hmacaddress"] not in macs:
+                            macs[data["hmacaddress"]] = 0
+                            if data["building"] == building_no:
+                                macs[data["hmacaddress"]] = 1
+                                total += 1
+                f.close()
+            except:
+                error_cnt += 1
+            i -= 1
+
+        results[0]["properties"]["total"] += total
+
+        if int(et) < 200:
+            s_time = end_time - 7800
+        else:
+            s_time = end_time - 200
+        j = end_time
+
+        while j > s_time:
+            try:
+                filePath = "PerMinute/0" + str(j)
+                with open(filePath) as f:
+                    a = f.readlines()
+                    for line in a:
+                        data = json.loads(line)
+                        if data["hmacaddress"] in macs and macs[data["hmacaddress"]] == 1:
+                            if data["building"] != building_no:
+                                if data["building"] not in cnt_res:
+                                    cnt_res[data["building"]] = 1
+                                else:
+                                    cnt_res[data["building"]] += 1
+                            macs[data["hmacaddress"]] = 0
+                f.close()
+            except:
+                error_cnt += 1
+            j -= 1
+
+    print("total:", results[0]["properties"]["total"])
 
     sorted_res = sorted(cnt_res.items(), key=lambda item:item[1], reverse=True)
 
